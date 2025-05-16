3fffb236eb9fd263860a7a45a447dfb586cfabcf
Sandro sulava <sandrosulava2@gmail.com>
2025-05-16 19:35:32 +0400

    feature

---
+++ script.js
@@ -0,0 +1,19 @@
+// const firstname = "sandro";
+
+// console.log(firstname);
+
+// firstname = "giorgi";
+
+// console.log(firstname);
+
+// ? string / number / boolean / undefind / null
+
+// ! console.log(firstname);
+
+// ! console.log(lastname);
+
+// ! console.log(age);
+
+let myButton = document.getElementById("myBtn");
+
+myButton.addEventListener("click");
